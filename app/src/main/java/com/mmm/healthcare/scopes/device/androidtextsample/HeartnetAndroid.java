70857aece0649f9060249e0e9cd6c38c451b00b3
Ahmed Imtiaz Humayun <32792313+AhmedImtiazPrio@users.noreply.github.com>
2019-01-10 19:20:00 +0600

    Update and rename AndroidTextSample.java to HeartnetAndroid.java

--- app/src/main/java/com/mmm/healthcare/scopes/device/androidtextsample/HeartnetAndroid.java
+++ app/src/main/java/com/mmm/healthcare/scopes/device/androidtextsample/HeartnetAndroid.java
@@ -1,10 +1,3 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 3M and its licensors 2013. All Rights Reserved. This software and         //
-// associated files are licensed under the terms of the signed license agreement.  All     //
-// sample code & sample applications are provided for demonstration purposes only and      //
-// should not be used for commercial or diagnostic purposes.                               //
-/////////////////////////////////////////////////////////////////////////////////////////////
-
 package com.mmm.healthcare.scopes.device.androidtextsample;
 
 import java.io.IOException;
